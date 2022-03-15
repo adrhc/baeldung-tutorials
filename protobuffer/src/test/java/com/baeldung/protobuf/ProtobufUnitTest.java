06265de1a3bbb472cdeffed65cad2d12a4eb5d56
Adrian Petre <adrian.petre@endava.com>
2022-03-15 15:34:38 +0200

    more complex addressbook.proto with proto3 protobuf version 3.19.4

--- protobuffer/src/test/java/com/baeldung/protobuf/ProtobufUnitTest.java
+++ protobuffer/src/test/java/com/baeldung/protobuf/ProtobufUnitTest.java
@@ -3,23 +3,19 @@ package com.baeldung.protobuf;
 import org.junit.After;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
 public class ProtobufUnitTest {
-	private final String filePath = "address_book";
-
-	@After
-	public void cleanup() throws IOException {
-		Files.deleteIfExists(Paths.get(filePath));
-	}
-
 	@Test
 	public void givenGeneratedProtobufClass_whenCreateClass_thenShouldCreateJavaInstance() {
 		//when
@@ -38,12 +34,11 @@ public class ProtobufUnitTest {
 		assertEquals(person.getName(), name);
 	}
 
-
 	@Test
 	public void givenAddressBookWithOnePerson_whenSaveAsAFile_shouldLoadFromFileToJavaClass() throws IOException {
 		//given
 		String email = "j@baeldung.com";
-		int id = new Random().nextInt();
+		int id = 13;
 		String name = "Michael Program";
 		Person person =
 				Person.newBuilder()
@@ -54,15 +49,17 @@ public class ProtobufUnitTest {
 
 		//when
 		AddressBook addressBook = AddressBook.newBuilder().addPeople(person).build();
-		FileOutputStream fos = new FileOutputStream(filePath);
-		addressBook.writeTo(fos);
-		fos.close();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		addressBook.writeTo(outputStream);
+
+		String base64 = Base64.getEncoder().encodeToString(outputStream.toByteArray());
+		System.out.println(base64);
+//		base64 = "CiMKD01pY2hhZWwgUHJvZ3JhbRANGg5qQGJhZWxkdW5nLmNvbQ==";
+		byte[] bytes = Base64.getDecoder().decode(base64);
 
 		//then
-		FileInputStream fis = new FileInputStream(filePath);
 		AddressBook deserialized =
-				AddressBook.newBuilder().mergeFrom(fis).build();
-		fis.close();
+				AddressBook.newBuilder().mergeFrom(bytes).build();
 		assertEquals(deserialized.getPeople(0).getEmail(), email);
 		assertEquals(deserialized.getPeople(0).getId(), id);
 		assertEquals(deserialized.getPeople(0).getName(), name);
